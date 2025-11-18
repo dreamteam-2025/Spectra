bff847a7907afd0eb5c07c2b9c00254e84ec72ca
Ostap Poliakov <cortezos@yandex.ru>
2025-11-18 16:38:43 +0300

    Add storybook

---
+++ src/shared/ui/Card/Card.stories.tsx
@@ -0,0 +1,15 @@
+import { Card } from "./Card";
+
+const meta = {
+  title: "Card stories",
+  component: Card,
+};
+
+export default meta;
+
+export const DefaultCard = {
+  args: {
+    width: 100,
+    height: 100,
+  },
+};
