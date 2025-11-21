3995fd048d332e7490af0edcfaf5c346ac61ee90
inevadim <kvadrocheyk@gmail.com>
2025-11-21 13:22:46 +0300

    delete src\shared\ui\Typography. Add google font Inter in body. Styled color link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"></link>
+      </head>
       <body>{children}</body>
     </html>
   );
