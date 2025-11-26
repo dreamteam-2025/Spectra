d3b9920fcf762b138f2c30537d403edc27b01878
Alexey Ryashin <fullnamemillie@gmail.com>
2025-11-26 23:34:11 +0300

    refactor(app/page): remove unused Ðьmymponent from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Card } from "@/shared/ui/Card/Card";
 import styles from "./page.module.scss";
-import { TextArea } from "@/shared/ui/TextArea/TextArea";
 
 export default function Home() {
   return (
@@ -8,7 +7,6 @@ export default function Home() {
       <main>
         <h1>Spectra. Main Page</h1>
         <Card width={100} height={100} />
-        <TextArea label={"label"} placeholder={"Enter title"} />
       </main>
     </>
   );
