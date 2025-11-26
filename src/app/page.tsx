473fd803c83fb7caf3aaa2c796fafc9c30300e50
Alexey Ryashin <fullnamemillie@gmail.com>
2025-11-26 23:26:15 +0300

    feat(shared/textArea): add textArea component styles and stories; refactor(shared/button): update button component and stories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import { Card } from "@/shared/ui/Card/Card";
 import styles from "./page.module.scss";
-import { Input } from "@/shared/ui/Input/Input";
+import { TextArea } from "@/shared/ui/TextArea/TextArea";
 
 export default function Home() {
   return (
     <>
-      <main >
+      <main>
         <h1>Spectra. Main Page</h1>
         <Card width={100} height={100} />
+        <TextArea label={"label"} placeholder={"Enter title"} />
       </main>
     </>
   );
