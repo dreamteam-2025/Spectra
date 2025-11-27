ea399eab554fbf283e94a125b2b9751c5dd9a542
Ostap Poliakov <cortezos@yandex.ru>
2025-11-27 22:34:00 +0300

    Create CheckBox component based on radix-ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import { Card } from "@/shared/ui/Card/Card";
 import styles from "./page.module.scss";
-import { Input } from "@/shared/ui/Input/Input";
 
 export default function Home() {
   return (
     <>
-      <main >
+      <main>
         <h1>Spectra. Main Page</h1>
         <Card width={100} height={100} />
       </main>
