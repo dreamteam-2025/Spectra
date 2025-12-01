5e485a1b5aaa0ba15e0476cbd91d70880ae0b9ba
Alexey Ryashin <fullnamemillie@gmail.com>
2025-12-01 18:35:55 +0300

    fix(shared/textArea) fix forwardRef in the component

--- src/shared/ui/TextArea/TextArea.tsx
+++ src/shared/ui/TextArea/TextArea.tsx
@@ -1,4 +1,4 @@
-import { ComponentProps, useId } from "react";
+import { ComponentProps, forwardRef, useId } from "react";
 import s from "./TextArea.module.scss";
 import { Primitive } from "@radix-ui/react-primitive";
 
@@ -11,27 +11,29 @@ type Props = {
   disabled?: boolean;
 } & ComponentProps<"textarea">;
 
-export const TextArea = ({ label, id, error, wrapperClassName, className, disabled = false, ...rest }: Props) => {
-  const generatedId = useId();
-  const textareaId = id || generatedId;
+export const TextArea = forwardRef<HTMLTextAreaElement, Props>(
+  ({ label, id, error, wrapperClassName, className, disabled = false, ...rest }, ref) => {
+    const generatedId = useId();
+    const textareaId = id || generatedId;
 
-  const hasError = Boolean(error);
+    const hasError = Boolean(error);
 
-  const textAreaClasses = [s.textarea, hasError && !disabled ? s.error : "", disabled ? s.disabled : "", className]
-    .filter(Boolean)
-    .join(" ");
+    const textAreaClasses = [s.textarea, hasError && !disabled ? s.error : "", disabled ? s.disabled : "", className]
+      .filter(Boolean)
+      .join(" ");
 
-  const labelClasses = [s.label, disabled ? s.labelDisabled : ""].filter(Boolean).join(" ");
+    const labelClasses = [s.label, disabled ? s.labelDisabled : ""].filter(Boolean).join(" ");
 
-  return (
-    <div className={`${s.wrapper} ${wrapperClassName}`}>
-      {label && (
-        <Primitive.label htmlFor={textareaId} className={labelClasses}>
-          {label}
-        </Primitive.label>
-      )}
-      <textarea id={textareaId} className={textAreaClasses} disabled={disabled} {...rest} />
-      {hasError && !disabled && <Primitive.p className={s.errorText}>{error}</Primitive.p>}
-    </div>
-  );
-};
+    return (
+      <div className={`${s.wrapper} ${wrapperClassName}`}>
+        {label && (
+          <Primitive.label htmlFor={textareaId} className={labelClasses}>
+            {label}
+          </Primitive.label>
+        )}
+        <textarea ref={ref} id={textareaId} className={textAreaClasses} disabled={disabled} {...rest} />
+        {hasError && !disabled && <Primitive.p className={s.errorText}>{error}</Primitive.p>}
+      </div>
+    );
+  }
+);
