e609eafeac6591316bb9160039d284085adbbaa2
Hripsime Madoyan <h.vardoeva.95@gmail.com>
2025-12-05 01:11:33 +0400

    I1-72
    Изменила название пропса комоненты NotificationBell
    (пока что кастыль, в дальнейшем при реализации логики разберемся)

--- src/entities/notification/ui/NotificationBell.tsx
+++ src/entities/notification/ui/NotificationBell.tsx
@@ -4,13 +4,13 @@ import s from "./NotificationBell.module.scss";
 
 type Props = {
   count?: number;
-  clickHandler?: () => void;
+  clickHandlerAction?: () => void;
 };
 
-export function NotificationBell({ count = 0, clickHandler }: Props) {
+export function NotificationBell({ count = 0, clickHandlerAction }: Props) {
   const bellUrl = "/icons/bell.svg";
   return (
-    <div className={s.notificationIcon} onClick={clickHandler}>
+    <div className={s.notificationIcon} onClick={clickHandlerAction}>
       <img src={bellUrl} alt="Notification bell icon" />
       {count > 0 && <span className={s.badge}>{count}</span>}
     </div>
