b5b5fced4aa4eb5f64e49b7968c4909fa744fb3a
Hripsime Madoyan <h.vardoeva.95@gmail.com>
2025-12-08 18:41:02 +0400

    feat(auth): Addes Sign Up page

--- src/(pages)/auth/signup/model/signup.schemas.ts
+++ src/(pages)/auth/signup/model/signup.schemas.ts
@@ -1,17 +1,44 @@
 import * as z from "zod";
 
+const hasNumber = /[0-9]/;
+const hasLower = /[a-z]/;
+const hasUpper = /[A-Z]/;
+const hasSpecial = /[!"#$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]/;
+
 export const signupSchema = z
   .object({
-    username: z.string().min(3, { error: "Имя не должно быть меньше 3 символов!" }),
-    email: z.email("Невалидный email"),
-    password: z.string().min(3, { error: "Минимальное значение 3 символа" }),
-    confirmPassword: z.string(),
+    username: z
+      .string()
+      .trim()
+      .min(6, { error: "Minimum number of characters 6" })
+      .max(30, { error: "Maximum number of characters 30" }),
+    email: z
+      .string()
+      .trim()
+      .min(1, "Email is required")
+      .pipe(z.email("The email must match the format example@example.com")),
+    password: z
+      .string()
+      .trim()
+      .min(6, { error: "Minimum number of characters 6" })
+      .max(20, { error: "Maximum number of characters 20" })
+      .refine(v => hasNumber.test(v) && hasLower.test(v) && hasUpper.test(v) && hasSpecial.test(v), {
+        message: "Password must contain lowercase, uppercase, and a symbol",
+      }),
+    confirmPassword: z
+      .string()
+      .trim()
+      .min(6, { error: "Minimum number of characters 6" })
+      .max(20, { error: "Maximum number of characters 20" })
+      .refine(v => hasNumber.test(v) && hasLower.test(v) && hasUpper.test(v) && hasSpecial.test(v), {
+        message: "Password must contain lowercase, uppercase, and a symbol",
+      }),
     acceptTerms: z.boolean().refine(val => val === true, {
-      message: "Вам необходимо принять условия, чтобы продолжить!",
+      message: "You need to accept the terms to continue!",
     }),
   })
   .refine(data => data.password === data.confirmPassword, {
-    message: "Пароль должен совпадать",
+    message: "Passwords do not match.",
     path: ["confirmPassword"],
   });
 
--- src/(pages)/auth/signup/ui/SignUp.tsx
+++ src/(pages)/auth/signup/ui/SignUp.tsx
@@ -1,10 +1,13 @@
 "use client";
 
-import { Form } from "react-hook-form";
+import { Controller } from "react-hook-form";
 import { useForm } from "react-hook-form";
 import { SignUpInputs, signupSchema } from "../model/signup.schemas";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/shared";
+import { Button, Card, CheckBox, Input, ROUTES } from "@/shared";
+import s from "./SignUp.module.scss";
+import Link from "next/link";
+import clsx from "clsx";
 
 export const SignUp = () => {
   const {
@@ -12,22 +15,90 @@ export const SignUp = () => {
     handleSubmit,
     reset,
     control,
-    formState: { errors },
+    formState: { errors, isValid, touchedFields },
   } = useForm<SignUpInputs>({
     defaultValues: { username: "", email: "", password: "", confirmPassword: "", acceptTerms: false },
     resolver: zodResolver(signupSchema),
+    mode: "onChange",
   });
 
-  function onSubmit() {
-    alert("Form is submeted!");
+  const googleLogoUrl = "/icons/google.svg";
+  const githubLogoUrl = "/icons/github.svg";
+
+  function onSubmit(data: SignUpInputs) {
+    reset();
+    console.log("Submitted data:", data);
+    alert("Form submitted!");
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Button type="submit" variant="primary">
-        Sign Up
-      </Button>
-      <Button variant="ghost">Sign In</Button>
-    </form>
+    <Card className={s.container}>
+      <h1 className={clsx(s.title, s.center)}>Sign Up</h1>
+      <div className={clsx(s.logos, s.center)}>
+        <div onClick={() => alert("Logged in from google account")}>
+          <img src={googleLogoUrl} alt="Google Logo" />
+        </div>
+        <div onClick={() => alert("Logged in from github account")}>
+          <img src={githubLogoUrl} alt="Github Logo" />
+        </div>
+      </div>
+      <form noValidate onSubmit={handleSubmit(onSubmit)} className={s.form}>
+        <Input
+          fullWidth
+          label="Username"
+          {...register("username")}
+          error={touchedFields.username ? errors.username?.message : undefined}
+        ></Input>
+        <Input
+          fullWidth
+          label="Email"
+          {...register("email")}
+          error={touchedFields.email ? errors.email?.message : undefined}
+        ></Input>
+        <Input
+          fullWidth
+          type="password"
+          label="Password"
+          {...register("password")}
+          error={touchedFields.password ? errors.password?.message : undefined}
+        ></Input>
+        <Input
+          fullWidth
+          type="password"
+          label="Password confirmation"
+          {...register("confirmPassword")}
+          error={touchedFields.confirmPassword ? errors.confirmPassword?.message : undefined}
+        ></Input>
+
+        <Controller
+          control={control}
+          name="acceptTerms"
+          render={({ field }) => (
+            <CheckBox className={s.center} checked={field.value} onChange={field.onChange}>
+              <span className="small-text">
+                I agree to the&nbsp;
+                <Link href={ROUTES.APP.TERMS}>
+                  <span className={s.link}>Terms of Service</span>
+                </Link>
+                &nbsp;and&nbsp;
+                <Link href={ROUTES.APP.PRIVACY}>
+                  <span className={s.link}>Privacy Policy</span>
+                </Link>
+              </span>
+            </CheckBox>
+          )}
+        />
+
+        <Button type="submit" variant="primary" disabled={!isValid}>
+          Sign Up
+        </Button>
+
+        <div className={s.center}> Do you have an account? </div>
+
+        <Link className={s.center} href={ROUTES.AUTH.LOGIN}>
+          <Button variant="ghost">Sign In</Button>
+        </Link>
+      </form>
+    </Card>
   );
 };
