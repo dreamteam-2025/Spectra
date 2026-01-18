fd45ad1336d4e5d498e5e6de9d5174f429501b94
inevadim <kvadrocheyk@gmail.com>
2026-01-18 20:35:01 +0300

    fix validation

--- src/(pages)/auth/createNewPassword/model/validation.ts
+++ src/(pages)/auth/createNewPassword/model/validation.ts
@@ -5,7 +5,13 @@ export const passwordSchema = z
     password: z
       .string()
       .min(6, "Password must be at least 6 characters")
-      .max(20, "Password must be no more than 20 characters"),
+      .max(20, "Password must be no more than 20 characters")
+      .refine(value => /[a-z]/.test(value), "Password must contain at least one lowercase letter")
+      .refine(value => /[A-Z]/.test(value), "Password must contain at least one uppercase letter")
+      .refine(
+        value => /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/.test(value),
+        "Password must contain at least one special character"
+      ),
     confirmPassword: z.string(),
   })
   .refine(data => data.password === data.confirmPassword, {
