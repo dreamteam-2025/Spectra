85489bd8ecd11589d44cbb1329bb441443adbcc6
inevadim <78503414+inevadim@users.noreply.github.com>
2026-01-21 09:45:50 +0300

    Update page.tsx

--- src/app/(protected)/profile/page.tsx
+++ src/app/(protected)/profile/page.tsx
@@ -1,10 +1,8 @@
-import { SideBar } from "@/widgets/sideBar/ui/SideBar";
-
 export default function ProfilePage() {
   return (
     <div>
       {/* Profile page */}
-      <SideBar />
+     
     </div>
   );
 }
