d3c0e7aa090e2634ce55066b803fea956cfcd7b7
Martyshe <sapognikmisha@gmail.com>
2026-02-09 21:37:17 +0100

    модалка открывается при клике на create в sidebar

--- src/(pages)/create/ui/CreatePage.tsx
+++ src/(pages)/create/ui/CreatePage.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import { useState } from "react";
-import { Button } from "@/shared";
+import { useEffect, useState } from "react";
 import { CreatePostModal } from "@/features/post/ui/CreatePostModal/CreatePostModal";
 
 export function CreatePostPage() {
   const [open, setOpen] = useState(false);
 
+  // Открываем модалку сразу при переходе на страницу
+  useEffect(() => {
+    setOpen(true);
+  }, []);
+
   return (
     <main>
-      <h1>Create Post Page</h1>
-
-      <Button variant="primary" onClick={() => setOpen(true)}>
-        Open Create Post
-      </Button>
-
       <CreatePostModal open={open} onOpenChange={setOpen} />
     </main>
   );
