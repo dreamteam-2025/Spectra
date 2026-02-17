568456035f3eeadd1536193b56dece1049672a2b
Ostap Poliakov <cortezos@yandex.ru>
2026-02-18 00:58:12 +0300

    refactor(getPosts): adjust RTKQ getPosts request, adjust '/feed' visual behavior

--- src/(pages)/feed/ui/FeedPage.tsx
+++ src/(pages)/feed/ui/FeedPage.tsx
@@ -1,7 +1,10 @@
+import s from "./FeedPage.module.scss";
+
 export const Feed = () => {
   return (
-    <main>
+    <main className={s.mainContent}>
       <h1>Feed page</h1>
+      <p>В дальнейшем здесь должна быть лента рекомендаций для конкретного пользователя...</p>
     </main>
   );
 };
